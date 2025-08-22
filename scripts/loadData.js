b008a377389149c12a2650a1d163cc7ba863d4f7
Wayne Dupree <wdupree@hotmail.com>
2025-08-22 15:38:50 -0500

    Update fetch script

--- scripts/loadData.js
+++ scripts/loadData.js
@@ -1,18 +1,14 @@
-function LoadJsonFile() {
-  fetch("/data/projectList.json") // Fetch the JSON file
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to load JSON file");
-      }
-      return response.json(); // Parse JSON
-    })
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => {
-      console.error("Error loading JSON:", error);
-    });
-}
-
-const viewModel = ko.mapping.fromJS(LoadJsonFile());
-ko.applyBindings(viewModel);
+fetch("/data/projectList.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.json(); // Parse JSON from the response
+  })
+  .then((data) => {
+    const viewModel = ko.mapping.fromJS(data); // Load the data into the observable array
+    ko.applyBindings(viewModel);
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
