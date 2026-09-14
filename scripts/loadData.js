978469abb57bff48fd185d74ad23e16e3ab4792c
wdupree <wdupree@hotmail.com>
2026-09-14 17:37:00 +0000

    Automated minify of f6e3dffed5a774333bc4d84d890e360d33a498e2

--- scripts/loadData.js
+++ scripts/loadData.js
@@ -1,14 +1,1 @@
-fetch("/data/projectList.json")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Failed to load JSON file");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    const viewModel = ko.mapping.fromJS(data);
-    ko.applyBindings(viewModel);
-  })
-  .catch((error) => {
-    console.error("There was a problem with the fetch operation:", error);
-  });
+fetch("/data/projectList.json").then(o=>{if(!o.ok)throw new Error("Failed to load JSON file");return o.json()}).then(o=>{const e=ko.mapping.fromJS(o);ko.applyBindings(e)}).catch(o=>{console.error("There was a problem with the fetch operation:",o)});
